5d7ad622e8fd7aff562c3a3b30b9370827965966
Eric Chai <electromatter@gmail.com>
2017-06-07 12:12:20 -0400

    tabs to spaces

--- perm.py
+++ perm.py
@@ -4,248 +4,247 @@ import collections
 import random
 
 def gcd(*args):
-	if len(args) == 1 and hasattr(args[0], '__iter__'):
-		args = iter(args[0])
-	g = 0
-	for x in args:
-		while x > 0:
-			x, g = g % x, x
-	return g
+    if len(args) == 1 and hasattr(args[0], '__iter__'):
+        args = iter(args[0])
+    g = 0
+    for x in args:
+        while x > 0:
+            x, g = g % x, x
+    return g
 
 def lcm(*args):
-	if len(args) == 1 and hasattr(args[0], '__iter__'):
-		args = iter(args[0])
-	m = 1
-	for x in args:
-		m *= (x // gcd(x, m))
-	return m
+    if len(args) == 1 and hasattr(args[0], '__iter__'):
+        args = iter(args[0])
+    m = 1
+    for x in args:
+        m *= (x // gcd(x, m))
+    return m
 
 class FrozenDict(collections.abc.Mapping):
-	__slots__ = ('_map', '_hash')
+    __slots__ = ('_map', '_hash')
 
-	def __new__(cls, *args, **kwargs):
-		self = super().__new__(cls)
-		self._map = dict(*args, **kwargs)
-		self._hash = hash(frozenset(self._map))
-		return self
+    def __new__(cls, *args, **kwargs):
+        self = super().__new__(cls)
+        self._map = dict(*args, **kwargs)
+        self._hash = hash(frozenset(self._map))
+        return self
 
-	def __getitem__(self, key):
-		return self._map[key]
+    def __getitem__(self, key):
+        return self._map[key]
 
-	def __len__(self):
-		return len(self._map)
+    def __len__(self):
+        return len(self._map)
 
-	def __iter__(self):
-		return iter(self._map)
+    def __iter__(self):
+        return iter(self._map)
 
-	def __hash__(self):
-		return self._hash
+    def __hash__(self):
+        return self._hash
 
-	def __repr__(self):
-		return '%s.FrozenDict(%r)' % (__name__, self._map)
+    def __repr__(self):
+        return '%s.FrozenDict(%r)' % (__name__, self._map)
 
 class Permutation(FrozenDict):
-	__slots__ = ('_orbits', '_order')
+    __slots__ = ('_orbits', '_order')
 
-	def __new__(cls, *args, **kwargs):
-		self = super().__new__(cls, *args, **kwargs)
-		unseen = set(self.keys())
-		orbits = []
-		while unseen:
-			first = key = unseen.pop()
-			orbit = []
-			while True:
-				orbit.append(key)
-				key = self[key]
-				if key == first:
-					break
-				try:
-					unseen.remove(key)
-				except KeyError:
-					raise ValueError('mapping is not injective') from None
-			if len(orbit) > 1:
-				orbits.append(Cycle(orbit))
-		try:
-			orbits.sort()
-		except TypeError:
-			pass
-		if len(orbits) == 0:
-			return IDENTITY
-		elif len(orbits) == 1:
-			return orbits[0]
-		self._orbits = tuple(orbits)
-		self._order = lcm(orbit.order() for orbit in self._orbits)
-		return self
+    def __new__(cls, *args, **kwargs):
+        self = super().__new__(cls, *args, **kwargs)
+        unseen = set(self.keys())
+        orbits = []
+        while unseen:
+            first = key = unseen.pop()
+            orbit = []
+            while True:
+                orbit.append(key)
+                key = self[key]
+                if key == first:
+                    break
+                try:
+                    unseen.remove(key)
+                except KeyError:
+                    raise ValueError('mapping is not injective') from None
+            if len(orbit) > 1:
+                orbits.append(Cycle(orbit))
+        try:
+            orbits.sort()
+        except TypeError:
+            pass
+        if len(orbits) == 0:
+            return IDENTITY
+        elif len(orbits) == 1:
+            return orbits[0]
+        self._orbits = tuple(orbits)
+        self._order = lcm(orbit.order() for orbit in self._orbits)
+        return self
 
-	@classmethod
-	def from_product(cls, *args):
-		if len(args) == 1 and hasattr(args[0], '__iter__'):
-			args = args[0]
-		if len(args) == 0:
-			return IDENTITY
-		elif len(args) == 1:
-			return Cycle(args)
-		mapping = {}
-		for cycle in reversed(args):
-			mapping.update({k: mapping.get(v, v) for k, v in Cycle(cycle).items()})
-		return cls(mapping)
+    @classmethod
+    def from_product(cls, *args):
+        if len(args) == 1 and hasattr(args[0], '__iter__'):
+            args = args[0]
+        if len(args) == 0:
+            return IDENTITY
+        elif len(args) == 1:
+            return Cycle(args)
+        mapping = {}
+        for cycle in reversed(args):
+            mapping.update({k: mapping.get(v, v) for k, v in Cycle(cycle).items()})
+        return cls(mapping)
 
-	def order(self):
-		return self._order
+    def order(self):
+        return self._order
 
-	def orbits(self):
-		return self._orbits
+    def orbits(self):
+        return self._orbits
 
-	def orbit(self, key):
-		for orbit in self.orbits():
-			if key in orbit:
-				return orbit
-		return IDENTITY
+    def orbit(self, key):
+        for orbit in self.orbits():
+            if key in orbit:
+                return orbit
+        return IDENTITY
 
-	def __getitem__(self, key):
-		try:
-			return super().__getitem__(key)
-		except KeyError:
-			return key
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            return key
 
-	def __pow__(self, exp):
-		mapping = {}
-		for orbit in self.orbits():
-			mapping.update(orbit ** exp)
-		return Permutation(mapping)
+    def __pow__(self, exp):
+        mapping = {}
+        for orbit in self.orbits():
+            mapping.update(orbit ** exp)
+        return Permutation(mapping)
 
-	def __mul__(self, other):
-		mapping = dict(self)
-		mapping.update({k: mapping.get(v, v) for k, v in other.items()})
-		return Permutation(mapping)
+    def __mul__(self, other):
+        mapping = dict(self)
+        mapping.update({k: mapping.get(v, v) for k, v in other.items()})
+        return Permutation(mapping)
 
-	def __call__(self, key):
-		return self[key]
+    def __call__(self, key):
+        return self[key]
 
-	def __repr__(self):
-		return '%s.Permutation.from_product%r' % (__name__, self.orbits())
+    def __repr__(self):
+        return '%s.Permutation.from_product%r' % (__name__, self.orbits())
 
 class Cycle(Permutation):
-	__slots__ = ('_cycle')
+    __slots__ = ('_cycle')
 
-	def __new__(cls, *args):
-		if len(args) == 1 and hasattr(args[0], '__iter__'):
-			args = args[0]
-		args = tuple(args)
-		if len(args) <= 1:
-			return IDENTITY
-		try:
-			repeat_index = args.index(args[0], 1)
-		except ValueError:
-			repeat_index = len(args)
-		args, repeat = args[:repeat_index], args[repeat_index:]
-		mapping = dict(zip(args, args[1:] + args[:1]))
-		if len(mapping) < len(args) or set(zip(args, repeat[1:] + repeat[:1])) - mapping.items():
-			raise ValueError('cycle inconsistent (not injective)')
-		try:
-			least_index = args.index(min(args))
-			args = args[least_index:] + args[:least_index]
-		except TypeError:
-			pass
-		self = FrozenDict.__new__(cls, mapping)
-		self._cycle = args
-		return self
+    def __new__(cls, *args):
+        if len(args) == 1 and hasattr(args[0], '__iter__'):
+            args = args[0]
+        args = tuple(args)
+        if len(args) <= 1:
+            return IDENTITY
+        try:
+            repeat_index = args.index(args[0], 1)
+        except ValueError:
+            repeat_index = len(args)
+        args, repeat = args[:repeat_index], args[repeat_index:]
+        mapping = dict(zip(args, args[1:] + args[:1]))
+        if len(mapping) < len(args) or set(zip(args, repeat[1:] + repeat[:1])) - mapping.items():
+            raise ValueError('cycle inconsistent (not injective)')
+        try:
+            least_index = args.index(min(args))
+            args = args[least_index:] + args[:least_index]
+        except TypeError:
+            pass
+        self = FrozenDict.__new__(cls, mapping)
+        self._cycle = args
+        return self
 
-	def __iter__(self):
-		return iter(self._cycle)
+    def __iter__(self):
+        return iter(self._cycle)
 
-	def order(self):
-		return len(self)
+    def order(self):
+        return len(self)
 
-	def orbits(self):
-		return (self,)
+    def orbits(self):
+        return (self,)
 
-	def orbit(self, key):
-		if key in self:
-			return self
-		return IDENTITY
+    def orbit(self, key):
+        if key in self:
+            return self
+        return IDENTITY
 
-	def __pow__(self, exp):
-		order = self.order()
-		exp %= order
-		if exp == 0:
-			return IDENTITY
-		elif exp == 1:
-			return self
-		elif exp == order - 1:
-			return Cycle(reversed(self._cycle))
-		elif gcd(exp, order) == 1:
-			cycle = []
-			index = 0
-			for _ in range(order):
-				index  = (index + exp) % order
-				cycle.append(self._cycle[index])
-			return Cycle(cycle)
-		unseen = set(range(order))
-		cycles = []
-		while unseen:
-			index = first = unseen.pop()
-			cycle = []
-			while True:
-				cycle.append(self._cycle[index])
-				index = (index + exp) % order
-				if index == first:
-					break
-				unseen.remove(index)
-			if len(cycle) > 1:
-				cycles.append(cycle)
-		return Permutation.from_product(cycles)
+    def __pow__(self, exp):
+        order = self.order()
+        exp %= order
+        if exp == 0:
+            return IDENTITY
+        elif exp == 1:
+            return self
+        elif exp == order - 1:
+            return Cycle(reversed(self._cycle))
+        elif gcd(exp, order) == 1:
+            cycle = []
+            index = 0
+            for _ in range(order):
+                index  = (index + exp) % order
+                cycle.append(self._cycle[index])
+            return Cycle(cycle)
+        unseen = set(range(order))
+        cycles = []
+        while unseen:
+            index = first = unseen.pop()
+            cycle = []
+            while True:
+                cycle.append(self._cycle[index])
+                index = (index + exp) % order
+                if index == first:
+                    break
+                unseen.remove(index)
+            if len(cycle) > 1:
+                cycles.append(cycle)
+        return Permutation.from_product(cycles)
 
-	def __repr__(self):
-		return '%s.Cycle%r' % (__name__, self._cycle)
+    def __repr__(self):
+        return '%s.Cycle%r' % (__name__, self._cycle)
 
 class Identity(Cycle):
-	__slots__ = ()
+    __slots__ = ()
 
-	__new__ = FrozenDict.__new__
+    __new__ = FrozenDict.__new__
 
-	def __iter__(self):
-		return iter([])
+    def __iter__(self):
+        return iter([])
 
-	def order(self):
-		return 1
+    def order(self):
+        return 1
 
-	def orbits(self):
-		return ()
+    def orbits(self):
+        return ()
 
-	def orbit(self, key):
-		return self
+    def orbit(self, key):
+        return self
 
-	def __pow__(self, exp):
-		return self
+    def __pow__(self, exp):
+        return self
 
-	def __mul__(self, other):
-		return other
+    def __mul__(self, other):
+        return other
 
-	def __repr__(self):
-		return '%s.IDENTITY' % __name__
+    def __repr__(self):
+        return '%s.IDENTITY' % __name__
 
 IDENTITY = Identity()
 
 def _make_domain(*args, **kwargs):
-	if not args and not kwargs:
-		raise TypeError('expected arguments')
-	try:
-		return list(range(*args, **kwargs))
-	except TypeError:
-		pass
-	return list(*args, **kwargs)
+    if not args and not kwargs:
+        raise TypeError('expected arguments')
+    try:
+        return list(range(*args, **kwargs))
+    except TypeError:
+        pass
+    return list(*args, **kwargs)
 
 def random_permutation(*args, **kwargs):
-	rng = kwargs.pop('random', None)
-	x = _make_domain(*args, **kwargs)
-	y = list(x)
-	random.shuffle(y, rng)
-	return Permutation(zip(x, y))
+    rng = kwargs.pop('random', None)
+    x = _make_domain(*args, **kwargs)
+    y = list(x)
+    random.shuffle(y, rng)
+    return Permutation(zip(x, y))
 
 def random_cycle(*args, **kwargs):
-	rng = kwargs.pop('random', None)
-	x = _make_domain(*args, **kwargs)
-	random.shuffle(y, rng)
-	return Cycle(x)
-
+    rng = kwargs.pop('random', None)
+    x = _make_domain(*args, **kwargs)
+    random.shuffle(y, rng)
+    return Cycle(x)
--- primitive_root.py
+++ primitive_root.py
@@ -1,101 +1,100 @@
 import pyprimes
 
 def totient_factors_non_prime(n):
-	if n <= 1:
-		# Really it could be defined as zero?
-		# but that would break the factorization code
-		# that assumes that p >= 2
-		raise ValueError('totient of n < 2 undefined')
-	phi = 1
-	for p, e in pyprimes.factorise(n):
-		yield (p - 1), 1
-		if e > 1:
-			yield p, (e - 1)
+    if n <= 1:
+        # Really it could be defined as zero?
+        # but that would break the factorization code
+        # that assumes that p >= 2
+        raise ValueError('totient of n < 2 undefined')
+    phi = 1
+    for p, e in pyprimes.factorise(n):
+        yield (p - 1), 1
+        if e > 1:
+            yield p, (e - 1)
 
 def totient_factors(n):
-	factors = {}
-	for p, e in totient_factors_non_prime(n):
-		if e > 1:
-			factors[p] = factors.get(p, 0) + e
-		elif e == 1:
-			for p, e in pyprimes.factorise(p):
-				factors[p] = factors.get(p, 0) + e
-	return factors.items()
+    factors = {}
+    for p, e in totient_factors_non_prime(n):
+        if e > 1:
+            factors[p] = factors.get(p, 0) + e
+        elif e == 1:
+            for p, e in pyprimes.factorise(p):
+                factors[p] = factors.get(p, 0) + e
+    return factors.items()
 
 def eval_factors(factors, modulus=None):
-	val = 1
-	for p, e in factors:
-		val = val * pow(p, e, modulus)
-		if modulus is not None:
-			val %= modulus
-	return val
+    val = 1
+    for p, e in factors:
+        val = val * pow(p, e, modulus)
+        if modulus is not None:
+            val %= modulus
+    return val
 
 def totient(n):
-	return eval_factors(totient_factors_non_prime(n))
+    return eval_factors(totient_factors_non_prime(n))
 
 _UNSPECIFIED=object()
 
 class PrimitiveRootTest:
-	def __init__(self, modulus):
-		self.totient_factors = list(totient_factors(modulus))
-		self.totient = eval_factors(self.totient_factors)
-		self.modulus = modulus
-		self.primes = pyprimes.primes()
+    def __init__(self, modulus):
+        self.totient_factors = list(totient_factors(modulus))
+        self.totient = eval_factors(self.totient_factors)
+        self.modulus = modulus
+        self.primes = pyprimes.primes()
 
-	def __new__(cls, n=_UNSPECIFIED, modulus=_UNSPECIFIED):
-		if modulus is _UNSPECIFIED or n is _UNSPECIFIED:
-			return super(cls, cls).__new__(cls)
-		return cls(modulus)(n)
+    def __new__(cls, n=_UNSPECIFIED, modulus=_UNSPECIFIED):
+        if modulus is _UNSPECIFIED or n is _UNSPECIFIED:
+            return super(cls, cls).__new__(cls)
+        return cls(modulus)(n)
 
-	def __iter__(self):
-		return self
+    def __iter__(self):
+        return self
 
-	def __next__(self):
-		if self.primes is None:
-			raise StopIteration
+    def __next__(self):
+        if self.primes is None:
+            raise StopIteration
 
-		for p in self.primes:
-			if p > self.modulus:
-				self.primes = None
-				raise StopIteration
+        for p in self.primes:
+            if p > self.modulus:
+                self.primes = None
+                raise StopIteration
 
-			if self(p):
-				return p
+            if self(p):
+                return p
 
-	def __call__(self, n):
-		n %= self.modulus
+    def __call__(self, n):
+        n %= self.modulus
 
-		if n == 0:
-			return False
+        if n == 0:
+            return False
 
-		for p, _ in self.totient_factors:
-			if pow(n, self.totient // p, self.totient) == 1:
-				return False
+        for p, _ in self.totient_factors:
+            if pow(n, self.totient // p, self.totient) == 1:
+                return False
 
-		return True
+        return True
 
 primitive_roots = PrimitiveRootTest
 is_primitive_root = PrimitiveRootTest
 
 # WARNING: this is recursive
 def egcd(a, b):
-	if b == 0:
-		return (a, 1, 0)
-	q, r = divmod(a, b)
-	g, s, t = egcd(b, r)
-	return (g, t, s - q*t)
+    if b == 0:
+        return (a, 1, 0)
+    q, r = divmod(a, b)
+    g, s, t = egcd(b, r)
+    return (g, t, s - q*t)
 
 def gcd(a, b):
-	while b > 0:
-		a, b = b, a % b
-	return a
+    while b > 0:
+        a, b = b, a % b
+    return a
 
 def is_coprime(a, b):
-	return gcd(a, b) == 1
+    return gcd(a, b) == 1
 
 def mul_inv(n, mod):
-	g, x, _ = egcd(n, mod)
-	if g != 1:
-		raise ValueError('%i is not coprime to %i' % (n, mod))
-	return x % mod
-
+    g, x, _ = egcd(n, mod)
+    if g != 1:
+        raise ValueError('%i is not coprime to %i' % (n, mod))
+    return x % mod
